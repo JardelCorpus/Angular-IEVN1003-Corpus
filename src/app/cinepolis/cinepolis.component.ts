31d26ae56c4ea5d1f809cbb8a1c1a40ceab5106e
Corpus <jardel_corpus@hotmail.com>
2022-10-01 09:28:10 -0500

    Practica Cinepolis

--- src/app/cinepolis/cinepolis.component.ts
+++ src/app/cinepolis/cinepolis.component.ts
@@ -22,8 +22,9 @@ export class CinepolisComponent {
 Calcular(){
   if(this.boletos<=this.compradores*7){
     if(this.boletos<=2){
+      this.total=(this.boletos*12)-((this.boletos*12)*0.10)
       if(this.tarjeta=="si"){
-        this.prueba=(` ${this.nombre} Tienes un Descuento Adicional del 10%`+ "\n"+ `Total a pagar: $ ${(this.boletos*12)-((this.boletos*12)*0.10)}`);
+        this.prueba=(` ${this.nombre} Tienes un Descuento Adicional del 10%`+ "\n"+ `Total a pagar: $ ${this.total}`);
       }
       else{
         this.prueba=(` ${this.nombre} No hay descuento`+ "\n"+ `Total a pagar: $ ${this.boletos*12}`);
@@ -31,15 +32,17 @@ Calcular(){
     }
     if(this.boletos>5){
       if(this.tarjeta=="si"){
-        this.prueba=(` ${this.nombre} Tienes 15% de descuento Total a pagar: $ ${(this.boletos*12)-((this.boletos*12)*0.15)}`  +  "\n");
+        this.total=(this.boletos*12)-((this.boletos*12)*0.15)
+        this.prueba=(` ${this.nombre} Tienes un Descuento Adicional al de 15% del 10%`+ "\n"+ `Total a pagar: $ ${(this.total)-((this.total)*0.10)}`);
       }
       else{
-        this.prueba=(` ${this.nombre} Tienes un Descuento Adicional al de 15% del 10%`+ "\n"+ `Total a pagar: $ ${(this.boletos*12)-((this.boletos*12)*0.25)}`);
+        this.prueba=(` ${this.nombre} Tienes 15% de descuento Total a pagar: $ ${(this.boletos*12)-((this.boletos*12)*0.15)}`  +  "\n");
       }
     }
     if(this.boletos>=3 && this.boletos<=5){
       if(this.tarjeta=="si"){
-        this.prueba=(` ${this.nombre} Tienes un Descuento Adicional al de 10% del 10%`+ "\n"+ `Total a pagar: $ ${(this.boletos*12)-((this.boletos*12)*0.20)}`);
+        this.total=(this.boletos*12)-((this.boletos*12)*0.10)
+        this.prueba=(` ${this.nombre} Tienes un Descuento Adicional al de 10% del 10%`+ "\n"+ `Total a pagar: $ ${(this.total)-((this.total)*0.10)}`);
       }
       else{
         this.prueba=(` ${this.nombre} Tienes 10% de descuento Total a pagar: $ ${(this.boletos*12)-((this.boletos*12)*0.10)}`  +  "\n");
